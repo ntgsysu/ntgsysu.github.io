262e1a9f5fb6c52b1153db3656a62e517f655bfd
liviler <liviler@163.com>
2025-03-19 11:47:21 +0800

    update

--- article/journalclub/journalclub.js
+++ article/journalclub/journalclub.js
@@ -3,15 +3,15 @@ function renderHTMLFromJson(id){
     .then((response) => response.json())
     .then((dataJson) => {
         //渲染数据到HTML
-        document.getElementById('title').innerText ='Journal Club: ' + dataJson.title
+        document.getElementById('title').innerHTML ='Journal Club: ' + dataJson.title
         document.getElementById('report-reporter').innerText = dataJson.speaker
         document.getElementById('report-time').innerText = dataJson.startTime.replace('T',' ') + ' ~ ' + dataJson.endTime.replace('T',' ')
         document.getElementById('report-location').innerText = dataJson.room
         dataJson.pptname ? document.querySelector('#report-document>a').setAttribute('href','./files/'+dataJson.pptname) : document.querySelector('#report-document').innerText=''
-        document.getElementById('paper-title').innerText = dataJson.paperTitle
+        document.getElementById('paper-title').innerHTML = dataJson.paperTitle
         document.getElementById('paper-doi').setAttribute('href',dataJson.paperLink) 
         document.getElementById('paper-doi').innerText = dataJson.paperLink.split('/').slice(-1)
-        document.getElementById('paper-abstract').innerText = dataJson.paperAbstract
+        document.getElementById('paper-abstract').innerHTML = dataJson.paperAbstract
     });
 
 
